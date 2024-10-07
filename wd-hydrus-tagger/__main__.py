34615bbc2cfdee4d261c3d772bd9ffe94f2e7ce7
uwidev <btyang24@gmail.com>
2024-10-06 17:45:50 -0700

    feat: do not tag existing, separate thresh for chara, local support?
    Borked code will remain borked until, if ever, a refactor occurs.
    
    **BREAKING**
    These changes break all functionality except hydrus api batch. This is
    because of the changes to what gets returned when we interrogate, where
    instead of 2 elements we now return 3 elements.
    
    Easily fixable for another time, but honestly a refactor of processing
    tags from a file would just be better.
    
    Future work will look to refactor code...

--- wd-hydrus-tagger/__main__.py
+++ wd-hydrus-tagger/__main__.py
@@ -1,7 +1,8 @@
 import os.path
+from pathlib import Path
 
 import click
-from PIL import Image, ImageFile
+from PIL import Image, ImageFile, UnidentifiedImageError
 from . import interrogate
 import hydrus_api
 from io import BytesIO
@@ -31,6 +32,7 @@ kaomojis = [
     "||_||",
 ]
 
+
 @click.group()
 def cli():
     pass
@@ -39,70 +41,162 @@ def cli():
 @click.command()
 @click.argument("filename")
 @click.option("--cpu", default=False, help="Use CPU instead of GPU")
-@click.option("--model", default="wd-v1-4-vit-tagger-v2", help="The tagging model to use")
+@click.option(
+    "--model", default="wd-v1-4-vit-tagger-v2", help="The tagging model to use"
+)
 @click.option("--threshold", default=0.35, help="The threshhold to drop tags below")
 def evaluate(filename, cpu, model, threshold):
-    if not os.path.isfile('./model/' + model + '/info.json'):
+    if not os.path.isfile("./model/" + model + "/info.json"):
         raise ValueError("info.json not found in model folder!")
 
-    with open('./model/' + model + '/info.json') as json_f:
+    with open("./model/" + model + "/info.json") as json_f:
         modelinfo = json.load(json_f)
 
     integerator = interrogate.WaifuDiffusionInterrogator(
-        modelinfo['modelname'], # the name of the model for display purposes
-        modelinfo['modelfile'], # the filename of the model file
-        modelinfo['tagsfile'], # the filename of the tags file
-        model, # the folder storing the previous two files as well as the info file
-		modelinfo['ratingsflag'], # flag indicating whether model identifies content rating
-		modelinfo['numberofratings'], # amount of tags to consider for content rating if so
-        repo_id=modelinfo['source'], # source of the model, credit where credit is due
+        modelinfo["modelname"],  # the name of the model for display purposes
+        modelinfo["modelfile"],  # the filename of the model file
+        modelinfo["tagsfile"],  # the filename of the tags file
+        model,  # the folder storing the previous two files as well as the info file
+        modelinfo[
+            "ratingsflag"
+        ],  # flag indicating whether model identifies content rating
+        modelinfo[
+            "numberofratings"
+        ],  # amount of tags to consider for content rating if so
+        repo_id=modelinfo["source"],  # source of the model, credit where credit is due
     )
     integerator.load(cpu)
     image = Image.open(filename)
     ratings, tags = integerator.interrogate(image)
     rating = "none"
-    if modelinfo['ratingsflag']: 
-        ratings["none"] = 0.0 # assign none a value of zero so that rating comparison can still occur
+    if modelinfo["ratingsflag"]:
+        ratings["none"] = (
+            0.0  # assign none a value of zero so that rating comparison can still occur
+        )
         for key in ratings.keys():
             if ratings[key] > ratings[rating]:
                 rating = key
     clipped_tags = []
 
     for key in tags.keys():
-        if (tags[key] > threshold):
+        if tags[key] > threshold:
             clipped_tags.append(key)
     click.echo("rating: " + rating)
     click.echo("tags: " + ", ".join(clipped_tags))
 
 
 @click.command()
+@click.argument("path")
+@click.option("--cpu", default=False, help="Use CPU instead of GPU")
+@click.option(
+    "--model", default="wd-v1-4-vit-tagger-v2", help="The tagging model to use"
+)
+@click.option("--threshold", default=0.35, help="The threshhold to drop tags below")
+def evaluate_path(path, cpu, model, threshold):
+    path = Path(path)
+    click.echo(path)
+
+    if not os.path.isfile("./model/" + model + "/info.json"):
+        raise ValueError("info.json not found in model folder!")
+
+    with open("./model/" + model + "/info.json") as json_f:
+        modelinfo = json.load(json_f)
+
+    integerator = interrogate.WaifuDiffusionInterrogator(
+        modelinfo["modelname"],  # the name of the model for display purposes
+        modelinfo["modelfile"],  # the filename of the model file
+        modelinfo["tagsfile"],  # the filename of the tags file
+        model,  # the folder storing the previous two files as well as the info file
+        modelinfo[
+            "ratingsflag"
+        ],  # flag indicating whether model identifies content rating
+        modelinfo[
+            "numberofratings"
+        ],  # amount of tags to consider for content rating if so
+        repo_id=modelinfo["source"],  # source of the model, credit where credit is due
+    )
+    integerator.load(cpu)
+
+    valid_ext = (".webp", ".png", ".jpg", ".jpeg")
+    for file in path.iterdir():
+        click.echo(f"{file.name.lower()=}")
+        click.echo(f"{os.path.isfile(file)=}")
+
+        if not file.is_file():
+            continue
+
+        if not file.name.lower().endswith(valid_ext):
+            continue
+
+        click.echo(f"processing {file}")
+
+        image = Image.open(file)
+        ratings, tags = integerator.interrogate(image)
+        rating = "none"
+        if modelinfo["ratingsflag"]:
+            ratings["none"] = (
+                0.0  # assign none a value of zero so that rating comparison can still occur
+            )
+            for key in ratings.keys():
+                if ratings[key] > ratings[rating]:
+                    rating = key
+        clipped_tags = []
+
+        for key in tags.keys():
+            if tags[key] > threshold:
+                clipped_tags.append(key)
+        click.echo("rating: " + rating)
+        click.echo("tags: " + ", ".join(clipped_tags))
+
+        sidecar = file.with_suffix(".txt")
+        with open(sidecar, "w") as fp:
+            fp.write(", ".join(clipped_tags))
+
+
+@click.command()
 @click.argument("hash")
 @click.option("--token", help="The API token for your Hydrus server")
 @click.option("--cpu", default=False, help="Use CPU instead of GPU")
-@click.option("--model", default="SmilingWolf/wd-v1-4-vit-tagger-v2", help="The tagging model to use")
+@click.option(
+    "--model",
+    default="SmilingWolf/wd-v1-4-vit-tagger-v2",
+    help="The tagging model to use",
+)
 @click.option("--threshold", default=0.35, help="The threshhold to drop tags below")
-@click.option("--host", default="http://127.0.0.1:45869", help="The URL for your Hydrus server ")
-@click.option("--tag-service", default="A.I. Tags", help="The Hydrus tag service to add tags to")
-@click.option("--ratings-only", default=False, help="Strip all tags except for content rating")
+@click.option(
+    "--host", default="http://127.0.0.1:45869", help="The URL for your Hydrus server "
+)
+@click.option(
+    "--tag-service", default="A.I. Tags", help="The Hydrus tag service to add tags to"
+)
+@click.option(
+    "--ratings-only", default=False, help="Strip all tags except for content rating"
+)
 @click.option("--privacy", default=True, help="hides the tag output from the cli")
-def evaluate_api(hash, token, cpu, model, threshold, host, tag_service, ratings_only, privacy):
-    if not os.path.isfile('./model/' + model + '/info.json'):
+def evaluate_api(
+    hash, token, cpu, model, threshold, host, tag_service, ratings_only, privacy
+):
+    if not os.path.isfile("./model/" + model + "/info.json"):
         raise ValueError("info.json not found in model folder!")
 
-    with open('./model/' + model + '/info.json') as json_f:
+    with open("./model/" + model + "/info.json") as json_f:
         modelinfo = json.load(json_f)
 
-    if ratings_only and not modelinfo['ratingsflag']:
+    if ratings_only and not modelinfo["ratingsflag"]:
         raise ValueError("--ratings-only set, but model does not support ratings!")
 
     integerator = interrogate.WaifuDiffusionInterrogator(
-        modelinfo['modelname'], # the name of the model for display purposes
-        modelinfo['modelfile'], # the filename of the model file
-        modelinfo['tagsfile'], # the filename of the tags file
-        model, # the folder storing the previous two files as well as the info file
-		modelinfo['ratingsflag'], # flag indicating whether model identifies content rating
-		modelinfo['numberofratings'], # amount of tags to consider for content rating if so
-        repo_id=modelinfo['source'], # source of the model, credit where credit is due
+        modelinfo["modelname"],  # the name of the model for display purposes
+        modelinfo["modelfile"],  # the filename of the model file
+        modelinfo["tagsfile"],  # the filename of the tags file
+        model,  # the folder storing the previous two files as well as the info file
+        modelinfo[
+            "ratingsflag"
+        ],  # flag indicating whether model identifies content rating
+        modelinfo[
+            "numberofratings"
+        ],  # amount of tags to consider for content rating if so
+        repo_id=modelinfo["source"],  # source of the model, credit where credit is due
     )
     integerator.load(cpu)
     client = hydrus_api.Client(token, host)
@@ -110,8 +204,10 @@ def evaluate_api(hash, token, cpu, model, threshold, host, tag_service, ratings_
     image = Image.open(image_bytes)
     ratings, tags = integerator.interrogate(image)
     rating = "none"
-    if modelinfo['ratingsflag']: 
-        ratings["none"] = 0.0 # assign none a value of zero so that rating comparison can still occur
+    if modelinfo["ratingsflag"]:
+        ratings["none"] = (
+            0.0  # assign none a value of zero so that rating comparison can still occur
+        )
         for key in ratings.keys():
             if ratings[key] > ratings[rating]:
                 rating = key
@@ -119,98 +215,170 @@ def evaluate_api(hash, token, cpu, model, threshold, host, tag_service, ratings_
 
     if not ratings_only:
         for key in tags.keys():
-            if (tags[key] > threshold):
-                clipped_tags.append(key.replace("_", " ") if key not in kaomojis else key)
+            if tags[key] > threshold:
+                clipped_tags.append(
+                    key.replace("_", " ") if key not in kaomojis else key
+                )
 
     if not privacy:
         click.echo("rating: " + rating)
         click.echo("tags: " + ", ".join(clipped_tags))
 
-    if modelinfo['ratingsflag']: clipped_tags.append("rating:" + rating)
+    if modelinfo["ratingsflag"]:
+        clipped_tags.append("rating:" + rating)
     if ratings_only:
-        clipped_tags.append("ratings only " + modelinfo['modelname'] + " ai generated tags") # create tag specifying that content tags were excluded
+        clipped_tags.append(
+            "ratings only " + modelinfo["modelname"] + " ai generated tags"
+        )  # create tag specifying that content tags were excluded
     else:
-        clipped_tags.append(modelinfo['modelname'] + " ai generated tags") # create tag from given model name
-    client.add_tags(hashes=[hash], service_names_to_tags={
-        tag_service: clipped_tags
-    })
+        clipped_tags.append(
+            "ai_generated: " + modelinfo["modelname"] + " ai generated tags"
+        )  # create tag from given model name
+    client.add_tags(hashes=[hash], service_names_to_tags={tag_service: clipped_tags})
 
 
 @click.command()
 @click.argument("hashfile")
 @click.option("--token", help="The API token for your Hydrus server")
 @click.option("--cpu", default=False, help="Use CPU instead of GPU")
-@click.option("--model", default="wd-v1-4-vit-tagger-v2", help="The tagging model to use")
-@click.option("--threshold", default=0.35, help="The threshhold to drop tags below")
-@click.option("--host", default="http://127.0.0.1:45869", help="The URL for your Hydrus server ")
-@click.option("--tag-service", default="A.I. Tags", help="The Hydrus tag service to add tags to")
-@click.option("--ratings-only", default=False, help="Strip all tags except for content rating")
+@click.option(
+    "--model", default="wd-v1-4-vit-tagger-v2", help="The tagging model to use"
+)
+@click.option(
+    "--general", default=0.35, help="The threshhold to drop general tags below"
+)
+@click.option(
+    "--character", default=0.8, help="The threshhold to drop character tags below"
+)
+@click.option(
+    "--host", default="http://127.0.0.1:45869", help="The URL for your Hydrus server "
+)
+@click.option(
+    "--tag-service", default="A.I. Tags", help="The Hydrus tag service to add tags to"
+)
+@click.option(
+    "--ratings-only", default=False, help="Strip all tags except for content rating"
+)
 @click.option("--privacy", default=True, help="hides the tag output from the cli")
-def evaluate_api_batch(hashfile, token, cpu, model, threshold, host, tag_service, ratings_only, privacy):
+def evaluate_api_batch(
+    hashfile,
+    token,
+    cpu,
+    model,
+    general,
+    character,
+    host,
+    tag_service,
+    ratings_only,
+    privacy,
+):
     if not os.path.isfile(hashfile):
         raise ValueError("hashfile not found!")
-    if not os.path.isfile('./model/' + model + '/info.json'):
+    if not os.path.isfile("./model/" + model + "/info.json"):
         raise ValueError("info.json not found in model folder!")
 
-    with open('./model/' + model + '/info.json') as json_f:
+    with open("./model/" + model + "/info.json") as json_f:
         modelinfo = json.load(json_f)
 
-    if ratings_only and not modelinfo['ratingsflag']:
+    if ratings_only and not modelinfo["ratingsflag"]:
         raise ValueError("--ratings-only set, but model does not support ratings!")
 
     integerator = interrogate.WaifuDiffusionInterrogator(
-        modelinfo['modelname'], # the name of the model for display purposes
-        modelinfo['modelfile'], # the filename of the model file
-        modelinfo['tagsfile'], # the filename of the tags file
-        model, # the folder storing the previous two files as well as the info file
-		modelinfo['ratingsflag'], # flag indicating whether model identifies content rating
-		modelinfo['numberofratings'], # amount of tags to consider for content rating if so
-        repo_id=modelinfo['source'], # source of the model, credit where credit is due
+        modelinfo["modelname"],  # the name of the model for display purposes
+        modelinfo["modelfile"],  # the filename of the model file
+        modelinfo["tagsfile"],  # the filename of the tags file
+        model,  # the folder storing the previous two files as well as the info file
+        modelinfo[
+            "ratingsflag"
+        ],  # flag indicating whether model identifies content rating
+        modelinfo[
+            "numberofratings"
+        ],  # amount of tags to consider for content rating if so
+        repo_id=modelinfo["source"],  # source of the model, credit where credit is due
     )
     integerator.load(cpu)
     client = hydrus_api.Client(token, host)
     with open(hashfile) as hashfile_f:
         hashes = hashfile_f.readlines()
 
+    model_name = modelinfo["modelname"].lower()
+    signature = f"ai_tagged:{model_name} g{general*100:.0f} c{character*100:.0f}"
+    unable = []
     with click.progressbar(hashes) as bar:
+        unable = []
         for hash in bar:
-            click.echo(" processing: "+ hash)
+            hash = hash.strip()
+            service_id = (
+                "f1454ce45d8c13972a6b4d0d36771aaae11305cab6fd90d77d0d5f12b08d05b9"
+            )
+            tags: list[str] = client.get_file_metadata([hash])[0]["tags"][service_id][
+                "storage_tags"
+            ].get("0")
+
+            if tags:
+                for tag in tags:
+                    if model_name in tag:
+                        # click.echo(f" {hash} already tagged\n skipping...")
+                        continue
+
+            # click.echo(" processing: " + hash)
             image_bytes = BytesIO(client.get_file(hash).content)
-            image = Image.open(image_bytes)
-            ratings, tags = integerator.interrogate(image)
+            try:
+                image = Image.open(image_bytes)
+            except (UnidentifiedImageError, OSError):
+                # click.echo(f"{hash} is not a supported file format\n skipping...")
+                unable.append(hash)
+                continue
+            ratings, general_tags, character_tags = integerator.interrogate(image)
             rating = "none"
-            if modelinfo['ratingsflag']: 
-                ratings["none"] = 0.0 # assign none a value of zero so that rating comparison can still occur
+            if modelinfo["ratingsflag"]:
+                ratings["none"] = (
+                    0.0  # assign none a value of zero so that rating comparison can still occur
+                )
                 for key in ratings.keys():
                     if ratings[key] > ratings[rating]:
                         rating = key
             clipped_tags = []
 
             if not ratings_only:
-                for key in tags.keys():
-                    if (tags[key] > threshold):
-                        clipped_tags.append(key.replace("_", " ") if key not in kaomojis else key)
+                for key in general_tags.keys():
+                    if general_tags[key] > general:
+                        clipped_tags.append(
+                            key.replace("_", " ") if key not in kaomojis else key
+                        )
+                for key in character_tags.keys():
+                    if character_tags[key] > character:
+                        clipped_tags.append(
+                            key.replace("_", " ") if key not in kaomojis else key
+                        )
 
             if not privacy:
                 click.echo("rating: " + rating)
                 click.echo("tags: " + ", ".join(clipped_tags))
                 click.echo()
 
-            if modelinfo['ratingsflag']: clipped_tags.append("rating:" + rating)
+            if modelinfo["ratingsflag"]:
+                clipped_tags.append("rating:" + rating)
             if ratings_only:
-                clipped_tags.append("ratings only " + modelinfo['modelname'] + " ai generated tags") # create tag specifying that content tags were excluded
+                clipped_tags.append(
+                    "ratings only " + signature
+                )  # create tag specifying that content tags were excluded
             else:
-                clipped_tags.append(modelinfo['modelname'] + " ai generated tags")
+                clipped_tags.append(signature)
 
-            client.add_tags(hashes=[hash], service_names_to_tags={
-                tag_service: clipped_tags
-            })
+            client.add_tags(
+                hashes=[hash], service_names_to_tags={tag_service: clipped_tags}
+            )
+
+    unable_s = "\n".join(unable)
+    click.echo(f"\nUnable to process hashes:\n{unable_s}")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     Image.init()
     ImageFile.LOAD_TRUNCATED_IMAGES = True
     cli.add_command(evaluate)
     cli.add_command(evaluate_api)
     cli.add_command(evaluate_api_batch)
+    cli.add_command(evaluate_path)
     cli()
--- wd-hydrus-tagger/interrogate.py
+++ wd-hydrus-tagger/interrogate.py
@@ -10,25 +10,25 @@ from pathlib import Path
 from . import dbimutils
 from . import onnx_loader
 
-tag_escape_pattern = re.compile(r'([\\()])')
+tag_escape_pattern = re.compile(r"([\\()])")
 use_cpu = True
 
 if use_cpu:
-    tf_device_name = '/cpu:0'
+    tf_device_name = "/cpu:0"
 else:
-    tf_device_name = '/gpu:0'
+    tf_device_name = "/gpu:0"
 
 
 class WaifuDiffusionInterrogator:
     def __init__(
-            self,
-            name: str,
-            model_file: str,
-            tags_file: str,
-            folder: str,
-            ratingsflag: bool,
-            numberofratings: int,
-            **kwargs
+        self,
+        name: str,
+        model_file: str,
+        tags_file: str,
+        folder: str,
+        ratingsflag: bool,
+        numberofratings: int,
+        **kwargs,
     ) -> None:
         self.name = name
         self.model_file = model_file
@@ -41,47 +41,48 @@ class WaifuDiffusionInterrogator:
     def findpaths(self) -> Tuple[os.PathLike, os.PathLike]:
         print(f"Loading {self.name} model file from {self.kwargs['repo_id']}")
 
-        model_file = Path('./model/' + self.folder + '/' + self.model_file)
-        tags_file = Path('./model/' + self.folder + '/' + self.tags_file)
+        model_file = Path("./model/" + self.folder + "/" + self.model_file)
+        tags_file = Path("./model/" + self.folder + "/" + self.tags_file)
         return model_file, tags_file
 
-    def load(self, cpu) -> None:                             #im pretty sure this just checks for the right onnx runtime package and downloads it if its missing, no clue why this cant be done on initial setup
+    def load(
+        self, cpu
+    ) -> None:  # im pretty sure this just checks for the right onnx runtime package and downloads it if its missing, no clue why this cant be done on initial setup
         model_file, tags_file = self.findpaths()
 
         # only one of these packages should be installed at a time in any one environment
         # https://onnxruntime.ai/docs/get-started/with-python.html#install-onnx-runtime
         # TODO: remove old package when the environment changes?
-        if not onnx_loader.is_installed('onnxruntime'):
-            package = os.environ.get(
-                'ONNXRUNTIME_PACKAGE',
-                'onnxruntime-gpu'
-            )
+        if not onnx_loader.is_installed("onnxruntime"):
+            package = os.environ.get("ONNXRUNTIME_PACKAGE", "onnxruntime-gpu")
 
-            onnx_loader.run_pip(f'install {package}', 'onnxruntime')
+            onnx_loader.run_pip(f"install {package}", "onnxruntime")
 
         from onnxruntime import InferenceSession
 
         # https://onnxruntime.ai/docs/execution-providers/
         # https://github.com/toriato/stable-diffusion-webui-wd14-tagger/commit/e4ec460122cf674bbf984df30cdb10b4370c1224#r92654958
-        providers = ['CUDAExecutionProvider', 'CPUExecutionProvider']
+        providers = ["CUDAExecutionProvider", "CPUExecutionProvider"]
         if cpu:
             providers.pop(0)
 
+        print("Using the following providers, preferring the first:")
+        print(f"{providers=}")
+
         self.model = InferenceSession(str(model_file), providers=providers)
 
-        print(f'Loaded {self.name} model from {model_file}')
+        print(f"Loaded {self.name} model from {model_file}")
 
         self.tags = pd.read_csv(tags_file)
 
     def interrogate(
-            self,
-            image: Image
+        self, image: Image
     ) -> Tuple[
         Dict[str, float],  # rating confidents
-        Dict[str, float]  # tag confidents
+        Dict[str, float],  # tag confidents
     ]:
         # init model
-        if not hasattr(self, 'model') or self.model is None:
+        if not hasattr(self, "model") or self.model is None:
             self.load()
 
         # code for converting the image and running the model is taken from the link below
@@ -92,10 +93,10 @@ class WaifuDiffusionInterrogator:
         _, height, _, _ = self.model.get_inputs()[0].shape
 
         # alpha to white
-        image = image.convert('RGBA')
-        new_image = Image.new('RGBA', image.size, 'WHITE')
+        image = image.convert("RGBA")
+        new_image = Image.new("RGBA", image.size, "WHITE")
         new_image.paste(image, mask=image)
-        image = new_image.convert('RGB')
+        image = new_image.convert("RGB")
         image = np.asarray(image)
 
         # PIL RGB to OpenCV BGR
@@ -111,16 +112,16 @@ class WaifuDiffusionInterrogator:
         label_name = self.model.get_outputs()[0].name
         confidents = self.model.run([label_name], {input_name: image})[0]
 
-        tags = self.tags[:][['name']]
-        tags['confidents'] = confidents[0]
+        tags = self.tags[:][["name"]]
+        tags["confidents"] = confidents[0]
+
+        # first X items are for rating
+        ratings = dict()
         if self.ratingsflag:
-            # first X items are for rating
-            ratings = dict(tags[:self.numberofratings].values)
+            ratings = dict(tags[: self.numberofratings].values)
 
-            # rest are regular tags
-            tags = dict(tags[self.numberofratings:].values)
-        else: 
-            tags = dict(tags.values) # all tags are regular tags
-            ratings = None
+        # find cutoff for general tags and character tags
+        general_tags = dict(tags[self.tags.iloc[:, 2] == 0].values)
+        character_tags = dict(tags[self.tags.iloc[:, 2] == 4].values)
 
-        return ratings, tags
+        return ratings, general_tags, character_tags
